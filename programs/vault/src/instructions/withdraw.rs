9965105ceb98ef1b3827ab0025a5dac252a7a72b
Mike Olson <mike@madtownmining.com>
2025-07-06 22:02:25 -0500

    Add withdraw file

---
+++ programs/vault/src/instructions/withdraw.rs
@@ -0,0 +1,47 @@
+use crate::state::VaultState;
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+
+    #[account(
+        seeds = [b"state", signer.key().as_ref()],
+        bump = vault_state.state_bump,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Withdraw<'info> {
+    pub fn withdraw(&mut self, amount: u64) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            to: self.signer.to_account_info(),
+            from: self.vault.to_account_info(),
+        };
+
+        let signing_seeds = [
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ];
+        let seeds = [&signing_seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &seeds);
+
+        transfer(cpi_ctx, amount)
+    }
+}

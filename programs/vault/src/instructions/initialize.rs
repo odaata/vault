938985ed170391b15ee910d48e20755b4a78f8d8
Mike Olson <mike@madtownmining.com>
2025-07-08 09:13:13 -0500

    Add rent exempt to initialize.

--- programs/vault/src/instructions/initialize.rs
+++ programs/vault/src/instructions/initialize.rs
@@ -1,5 +1,7 @@
-use anchor_lang::prelude::*;
-
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
 use crate::{state::VaultState, ANCHOR_DISCRIMINATOR};
 
 #[derive(Accounts)]
@@ -26,6 +28,14 @@ pub struct Initialize<'info> {
 
 impl<'info> Initialize<'info> {
     pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        let rent_exempt: u64 = Rent::get()?.minimum_balance(self.vault.to_account_info().data_len());
+        let cpi_program = self.system_program.to_account_info();
+        let cpi_accounts = Transfer {
+            from: self.signer.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        transfer(cpi_ctx, rent_exempt)?;
         self.vault_state.state_bump = bumps.vault_state;
         self.vault_state.vault_bump = bumps.vault;
         Ok(())
